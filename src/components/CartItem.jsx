4a486c961ee77e66da189fdd54697e6f24713fbe
mdgsaruar710344 <golamsaruar004@gmail.com>
2024-11-15 11:20:57 +0600

    added delete button and set minimum input value in update cart Quantity

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,44 +2,69 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopProvider";
 
 
-const CartItem = ({item}) => {
- const {products,cart,setCart,setCartQuantity}= useContext(ShopContext)
-  const {ID,Size,Quantity}=item;
-  const [matchedCartItem,setMatchedCartItem]=useState()
-  const [itemTotalPrice,setItemTotalPrice]=useState(0)
-  
+const CartItem = ({ item }) => {
+  const { products, cart, setCart, setCartQuantity } = useContext(ShopContext)
+  const { ID, Size, Quantity } = item;
+  const [matchedCartItem, setMatchedCartItem] = useState()
+  const [itemTotalPrice, setItemTotalPrice] = useState(0)
+  const [inputValue,setInputValue]=useState()
 
-useEffect(()=>{
 
-  for(const product of products ){
-    const matchingProduct= product.id==ID ? product : '';
-       if(matchingProduct){
+  useEffect(() => {
+
+    for (const product of products) {
+      const matchingProduct = product.id == ID ? product : '';
+      if (matchingProduct) {
         setMatchedCartItem(matchingProduct);
-       }
+      }
+    }
+
+  }, [products, cart])
+
+  // useEffect(() => {
+  //   console.log(matchedCartItem)
+  //   const totalitemprice = Quantity * matchedCartItem.price;
+  //   setItemTotalPrice(totalitemprice);
+  // }, [matchedCartItem])
+
+  useEffect(() => {
+    if(matchedCartItem){
+      const totalitemprice = Quantity * matchedCartItem.price;
+      console.log(totalitemprice);
+      setItemTotalPrice(totalitemprice);
+    }
+  
+  }, [cart,matchedCartItem])
+
+
+
+
+  const handleUpdateQuantity = (e) => {
+    const newquantity = parseInt(e.target.value);
+    console.log(newquantity);
+    if(newquantity>0){
+      const QuantityUpdatedcart = cart.map(item => item.ID == ID && item.Size == Size ? { ...item, Quantity: newquantity } : item)
+      console.log(QuantityUpdatedcart);
+  
+      setCart(QuantityUpdatedcart);
     }
  
-  },[products,cart])
 
-  useEffect(()=>{
-    console.log(matchedCartItem)
-    const totalitemprice=Quantity*matchedCartItem.price;
-    setItemTotalPrice(totalitemprice);
-  },[matchedCartItem])
+  }
 
+  const handleDeleteItem=()=>{
+    const deleteUpdatedCart=cart.filter(item=> !(item.ID == ID && item.Size == Size));
+    console.log(deleteUpdatedCart);
+    setCart(deleteUpdatedCart);
+  }
 
-  
-  
-
-
-  const handleUpdateQuantity=(e)=>{
-    const newquantity= parseInt(e.target.value);
-    console.log(newquantity);
-   const QuantityUpdatedcart= cart.map(item=> item.ID==ID && item.Size==Size ? {...item, Quantity:newquantity} :item)
-   console.log(QuantityUpdatedcart);
-   setCart(QuantityUpdatedcart);
-   const totalitemprice=Quantity*matchedCartItem.price;
-   console.log(totalitemprice);
-  setItemTotalPrice(totalitemprice);
+  const handleInputValue=(e)=>{
+    if(e.target.value===''|| e.target.value<1){
+        setInputValue(1)
+    }
+    else{
+      setInputValue(e.target.value);
+    }
   }
 
   useEffect(() => {
@@ -51,7 +76,7 @@ useEffect(()=>{
 
     setCartQuantity(newTotalvalue);
 
-  }, [cart])
+  }, [cart,handleDeleteItem])
 
   return (
     <div className="m-4 border-red-600 border-2 flex flex-row">
@@ -63,22 +88,27 @@ useEffect(()=>{
       <br></br>
       {matchedCartItem && <div>
         Title:
-         {matchedCartItem.title}
-         <br></br>
-         <div className="">
-         <img className="w-[100px] h-[100px] m-5 object-cover" src={matchedCartItem.photo[0]}/> 
-         </div>
-         Update Quantity: 
-         <input onChange={handleUpdateQuantity} name="updatedquantity" type="number" defaultValue={Quantity} min={1} />
-                 
-         </div> }
+        {matchedCartItem.title}
+        <br></br>
+        <div className="">
+          <img className="w-[100px] h-[100px] m-5 object-cover" src={matchedCartItem.photo[0]} />
+        </div>
+        <div> price: {matchedCartItem.price}</div>
+        Update Quantity:
+        <input onChange={handleUpdateQuantity} onInput={handleInputValue} value={inputValue} name="updatedquantity" type="number" defaultValue={Quantity} min={1} />
 
-         <div>
-          Total Price:
-          {itemTotalPrice}
-          <br></br>
+       <div>
+        <button onClick={handleDeleteItem} className="btn btn-warning">Delete</button>
+       </div>
+      </div>}
 
-         </div>
+      <div>
+        Total Price:
+        {itemTotalPrice}
+
+        <br></br>
+
+      </div>
     </div>
   );
 };
